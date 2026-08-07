750d8e5a94ba9e74ae6ee7244f9b0de111b5b14a
khalodark <khalidqadamani@gmail.com>
2026-08-07 19:52:09 +0300

    add chat contoller code

---
+++ src/main/java/com/khalodark/ollama/controller/ChatController.kt
@@ -0,0 +1,17 @@
+package com.khalodark.ollama.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class ChatController {
+
+    @GetMapping("/")
+    fun home(model: Model): String {
+
+        return "index"
+
+    }
+
+}
